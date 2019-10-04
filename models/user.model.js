7f2917fb1203c8e727532dcc39b9a44b9fbef66d
Saarang Tiwari <saarang.tiwari@hotmail.com>
2019-10-04 10:10:56 +0530

    moved login to service from model

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Cryptic = require('../utils/cryptic');
-const UtilityService = require('../services/utilityService');
 
 // Create Schema
 const UserSchema = new Schema({
@@ -64,23 +63,7 @@ const UserModel = {
 
   findOneUser: async userData => await User.findOne(userData),
 
-  login: async user => {
-    const email = user.email;
-    const password = user.password;
-
-    // find previous data of the user:
-    const userData = await User.findOne({ email });
-    if (!userData) return { message: "User not found" };
-    const isMatch = await Cryptic.compare(password, userData.password);
-
-    if (!isMatch) return null; // user not found
-
-    // finding user data from db:
-    const token = await UtilityService.generateToken({ firstName: userData.firstName, lastName: userData.lastName, email: userData.email, phoneNumber: userData.phoneNumber });
-    return token;
-  },
-
-  getUserAboveAge: async age => await User.find({ age: { $gt: age } }).sort({birthdayTimeaStamp : -1})
+  getUserAboveAge: async age => await User.find({ age: { $gt: age } }).sort({birthdayTimeStamp : -1})
 }
 
 module.exports = UserModel;
--- services/user.service.js
+++ services/user.service.js
@@ -1,9 +1,25 @@
 const UserModel = require('../models/user.model');
+const UtilityService = require('../services/utilityService');
+const Cryptic = require('../utils/cryptic');
 
 const UserService = {
     addNewUser: async user => await UserModel.addNewUser(user),
     getAllUsers: async () => await UserModel.getAllUsers(),
-    login: async user => await UserModel.login(user),
+    login: async user => {
+        // await UserModel.login(user)
+        const email = user.email;
+        const password = user.password;
+        // find previous data of the user:
+        const userData = await UserModel.findOneUser({ email });
+        if (!userData) return { message: "User not found" };
+        const isMatch = await Cryptic.compare(password, userData.password);
+    
+        if (!isMatch) return null; // user not found
+    
+        // finding user data from db:
+        const token = await UtilityService.generateToken({ firstName: userData.firstName, lastName: userData.lastName, email: userData.email, phoneNumber: userData.phoneNumber });
+        return token;
+    },
     getUserAboveAge: async age => await UserModel.getUserAboveAge(age)
 };
 
